ae08e9d3474aaa40570ed268e44fc896a7380171
OrangeTacTics <>
2021-08-29 16:00:57 -0700

    Change dupilcates to remove 'duplicate' key from entries.

--- transforms/02_duplicates.py
+++ transforms/02_duplicates.py
@@ -9,9 +9,12 @@ with open('data/duplicates.json') as infile:
 
 for key, val in updates.items():
     if val['meaning']:
-        words[key]['meaning'] = val
+        words[key]['meaning'] = val['meaning']
     else:
         del words[key]
+
+for word in words.values():
+    del word['duplicate']
 
 with open(sys.argv[2], 'w') as outfile:
     json.dump(words, outfile, indent=4, ensure_ascii=False)
