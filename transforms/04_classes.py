86d7ca63c4df2cb4f5e33ff096a9dbe867a6a0e4
OrangeTacTics <>
2021-08-29 18:34:41 -0700

    Make classes more convenient to create.

--- transforms/04_classes.py
+++ transforms/04_classes.py
@@ -1,14 +1,25 @@
 import json
 import sys
 
+CLASSES = {}
 
+
+def cls(fn):
+    name = fn.__name__[3:].upper()
+    CLASSES[name] = fn
+
+
+@cls
 def is_number(word):
     return word['simplified'] in '一ニ三四五六七八九十百千 万億兆'
 
 
+@cls
 def is_counter(word):
     return 'measure word' in word['meaning'] or '(mw ' in word['meaning']
 
+
+@cls
 def is_grammar(word):
     return word['simplified'] in [
         '的',
@@ -16,6 +27,7 @@ def is_grammar(word):
     ]
 
 
+@cls
 def is_pronoun(word):
     return word['simplified'] in '我们你您他她它'
 
@@ -25,19 +37,11 @@ with open(sys.argv[1]) as infile:
 
 for word in words.values():
     classes = []
-    if is_number(word):
-        classes.append('NUMBER')
+    for cls_name, cls_predicate in CLASSES.items():
+        if cls_predicate(word):
+            classes.append(cls_name)
 
-    if is_counter(word):
-        classes.append('COUNTER')
-
-    if is_grammar(word):
-        classes.append('GRAMMAR')
-
-    if is_pronoun(word):
-        classes.append('PRONOUN')
-
-    word['classes'] = classes
+    word['classes'] = sorted(classes)
 
 with open(sys.argv[2], 'w') as outfile:
     json.dump(words, outfile, indent=4, ensure_ascii=False)
